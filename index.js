0790b5a4bbd26e8814f07b230a7c9407d1557233
thestreaky <jiachaowu@hotmail.com>
2021-04-10 01:53:48 -0400

    up

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const sockerio=require('socket.io');
+const socketio=require('socket.io');
 const http = require('http');
+//const cors = require('cors');
+
 
 
 const PORT = process.env.PORT||5000;
 
 const router = require('./router');
-const app = express()
+const app = express();
 const server = http.createServer(app);
-const io = sockerio(server);
+//app.use(cors())
+const io = socketio(server);
+// const io = require("socket.io")(http, {
+//     cors: {
+//       origin: "localhost:3000",
+//       methods: ["GET", "POST"],
+//       allowedHeaders: ["my-custom-header"],
+//       credentials: true
+//     }
+//   });
 
 io.on('connection', (socket)=>{
     console.log('We have a new connection!!!');
     socket.on('disconnect',()=>{
-        console.log('User had left!!!')
-    })
+        console.log('User had left!!!');
+    });
 })
 app.use(router);
 server.listen(PORT, ()=>console.log(`Server is running and up ${PORT}`))
