abb912dfbb9254e3769e18494ae7c3d23e45eb04
thestreaky <jiachaowu@hotmail.com>
2021-04-11 05:04:14 -0400

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const socketio=require('socket.io');
 const http = require('http');
 //const cors = require('cors');
 
-const {addUser, removeUser,getUser,getUserInRoom} =require('./users');
+const {addUser, removeUser,getUser,getUsersInRoom} =require('./users');
 
 
 const PORT = process.env.PORT||5000;
@@ -18,18 +18,20 @@ corsOptions={
 }
 const io = socketio(server,corsOptions);
 
-
 io.on('connection', (socket)=>{
     socket.on('join',({name,room},callback)=>{
         const {error,user} = addUser({id: socket.id,name,room});
         if(error){
             return callback(error);
         }
-        socket.emit('message',{user:'admin',text: `${name,room},welcome to the room ${room}`})
+        socket.emit('message',{user:'admin',text: `${name},welcome to the room ${room}`})
         socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name},has joined!`})
         socket.join(user.room);
 
         console.log(socket.id,name,room);
+
+        io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
+
         callback();
     });
 
@@ -37,13 +39,19 @@ io.on('connection', (socket)=>{
         const user = getUser(socket.id);
         
         io.to(user.room).emit('message',{user:user.name,text:message});
-
+        
+        io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
+       
         callback();
 
     });
 
     socket.on('disconnect',()=>{
         console.log('User had left!!!');
+        const user = removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('message',{user:'admin',text:`${user.name}`})
+        }
     });
 })
 app.use(router);
